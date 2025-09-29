959aa71ee0305444648201a205a232bd31fbd4b8
david.bloomfield <david.s.bloomfield@gmail.com>
2025-09-29 10:28:07 -0600

    add logout test

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -4,6 +4,15 @@ const app = require('../service');
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
 
+jest.setTimeout(60 * 1000 * 5); // 5 minutes
+
+const loginUser = async (user) => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+
+  return loginRes.body;
+}
+
 beforeAll(async () => {
     testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
     const registerRes = await request(app).post('/api/auth').send(testUser);
@@ -12,14 +21,22 @@ beforeAll(async () => {
 });
 
 test('login', async () => {
-    const loginRes = await request(app).put('/api/auth').send(testUser);
-    expect(loginRes.status).toBe(200);
-    expectValidJwt(loginRes.body.token);
+    const responseBody = await loginUser(testUser);
+    expectValidJwt(responseBody.token);
 
     const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
     delete expectedUser.password;
-    expect(loginRes.body.user).toMatchObject(expectedUser);
+    expect(responseBody.user).toMatchObject(expectedUser);
 });
+
+test('logout', async () => {
+  const loginBody = await loginUser(testUser);
+  const logoutRes = await request(app).delete('/api/auth').set({Authorization: `Bearer ${loginBody.token}`}).send(testUser);
+  expect(logoutRes.status).toBe(200);
+
+  expect(logoutRes.body.message).toEqual('logout successful');
+})
+
 
 function expectValidJwt(potentialJwt) {
     expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
