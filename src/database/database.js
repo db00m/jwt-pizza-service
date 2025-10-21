c7328fae6a39f45302b0c7a4e1b10c733aedd1a9
david.bloomfield <david.s.bloomfield@gmail.com>
2025-10-21 16:33:26 -0600

    fix linting issues

--- src/database/database.js
+++ src/database/database.js
@@ -133,7 +133,7 @@ class DB {
       await this.query(connection, 'DELETE FROM userRole WHERE userId=?', [userId]);
       await this.query(connection, 'DELETE FROM user WHERE id=?', [userId]);
       await connection.commit();
-    } catch(e) {
+    } catch {
       await connection.rollback();
       throw new StatusCodeError('unable to delete user', 500);
     } finally {
