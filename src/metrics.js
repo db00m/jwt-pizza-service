bdeb725fe84f8e91a2b970aa149bbe49745f6ce9
david.bloomfield <david.s.bloomfield@gmail.com>
2025-11-05 20:38:54 -0700

    Add request and authentication metric tracking

---
+++ src/metrics.js
@@ -0,0 +1,100 @@
+const config = require('./config');
+
+// Create data structures to hold metrics
+const requests = {};
+const authAttempts = {};
+
+// Create functions to expose in middleware that update metrics data structures
+
+// HTTP request metric
+function requestTracker(req, res, next) {
+  const method = `${req.method}`;
+  requests[method] = (requests[method] || 0) + 1;
+  next();
+}
+
+// Active user metric
+// Authentication Attempts
+function authenticationTracker(authStatus) {
+  authAttempts[authStatus] = (authAttempts[authStatus] || 0) + 1
+}
+// CPU and Memory
+// Pizzas Sold
+// Latency
+
+// Build OTel request body
+
+setInterval(() => {
+  const metrics = [];
+  Object.keys(requests).forEach((method) => {
+    metrics.push(createMetric('requests', requests[method], '1', 'sum', 'asInt', { method }));
+  });
+  Object.keys(authAttempts).forEach((status) => {
+    metrics.push(createMetric('authentications', authAttempts[status], '1', 'sum', 'asInt', { status }));
+  });
+
+  sendMetricToGrafana(metrics);
+}, 10000);
+
+function createMetric(metricName, metricValue, metricUnit, metricType, valueType, attributes) {
+  attributes = { ...attributes, source: config.metrics.source };
+
+  const metric = {
+    name: metricName,
+    unit: metricUnit,
+    [metricType]: {
+      dataPoints: [
+        {
+          [valueType]: metricValue,
+          timeUnixNano: Date.now() * 1000000,
+          attributes: [],
+        },
+      ],
+    },
+  };
+
+  Object.keys(attributes).forEach((key) => {
+    metric[metricType].dataPoints[0].attributes.push({
+      key: key,
+      value: { stringValue: attributes[key] },
+    });
+  });
+
+  if (metricType === 'sum') {
+    metric[metricType].aggregationTemporality = 'AGGREGATION_TEMPORALITY_CUMULATIVE';
+    metric[metricType].isMonotonic = true;
+  }
+
+  return metric;
+}
+
+function sendMetricToGrafana(metrics) {
+  const body = {
+    resourceMetrics: [
+      {
+        scopeMetrics: [
+          {
+            metrics,
+          },
+        ],
+      },
+    ],
+  };
+
+  fetch(`${config.metrics.url}`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: { Authorization: `Bearer ${config.metrics.apiKey}`, 'Content-Type': 'application/json' },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP status: ${response.status}`);
+      }
+    })
+    .catch((error) => {
+      console.error('Error pushing metrics:', error);
+    });
+}
+// Send metrics to Grafana periodically
+
+module.exports = { requestTracker, authenticationTracker };
